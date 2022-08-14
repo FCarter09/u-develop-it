f58a2442ce5840c247faa6b0b1b3d42f02316f0a
Forest Carter <fc_vstate@yahoo.com>
2022-08-14 19:02:43 -0400

    feature/cantidates

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-
+USE election;
 CREATE TABLE candidates (
-    id INTEGER PRIMARY KEY NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     industry_connected BOOLEAN NOT NULL
